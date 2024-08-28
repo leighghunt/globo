6df3e284dc64008b4fdfb67605b5f189f752cb6b
Leigh Hunt <leigh@venari.co.nz>
2024-08-28 21:18:45 +1200

    First steps - Application and Component

---
+++ ReactWeb/src/main/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header.tsx';
+
+function App() {
+
+  return (
+    <div className="container">
+            <Header subtitle="BLAH"/>
+    </div>
+  )
+}
+
+export default App
