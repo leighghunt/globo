e3ef69d4d409f624953716b9d877981e7f9f33f6
Leigh Hunt <leigh@venari.co.nz>
2024-08-29 21:44:14 +1200

    HouseDetail

--- Api/Program.cs
+++ Api/Program.cs
@@ -29,10 +29,16 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/houses", (IHouseRepository repository) =>
-    repository.GetAll()
-)
-// .WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/houses", (IHouseRepository repository) => repository.GetAll()).Produces<List<HouseDto>>(StatusCodes.Status200OK);
+app.MapGet("/house/{houseId:int}", async (IHouseRepository repository, int houseId) => {
+    var house = await repository.Get(houseId);
+
+    if(house == null)
+    {
+        return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
+    }
+
+    return Results.Ok(house);
+}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
 
 app.Run();
