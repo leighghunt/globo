243ec7956828e790172ff5ff23066fea8c9f4002
Leigh Hunt <leigh@venari.co.nz>
2024-08-30 17:22:12 +1200

    CRUD in progress

--- Api/Data/HouseRepository.cs
+++ Api/Data/HouseRepository.cs
@@ -3,7 +3,10 @@ using Microsoft.EntityFrameworkCore;
 public interface IHouseRepository
 {
     Task<List<HouseDto>> GetAll();
-    Task<HouseDetailDto> Get(int houseId);
+    Task<HouseDetailDto?> Get(int houseId);
+    Task<HouseDetailDto> Add(HouseDetailDto house);
+    Task<HouseDetailDto> Update(HouseDetailDto house);
+    Task Delete(int houseId);
 }
 
 public class HouseRepository:IHouseRepository
@@ -28,6 +31,61 @@ public class HouseRepository:IHouseRepository
             return null;
         }
 
+        return EntityToDto(e);
+    }
+
+    private static HouseDetailDto EntityToDto(HouseEntity e)
+    {
         return new HouseDetailDto(e.Id, e.Address, e.Country, e.Price, e.Description, e.Photo);
+    }
+
+    private static void DtoToEntity(HouseDetailDto dto, HouseEntity entity)
+    {
+        entity.Address = dto.Address;
+        entity.Country = dto.Country;
+        entity.Price = dto.Price;
+        entity.Description = dto.Description;
+        entity.Photo = dto.Photo;
+    }
+
+    public async Task<HouseDetailDto> Add(HouseDetailDto house)
+    {
+        var e = new HouseEntity();
+        DtoToEntity(house, e);
+
+        context.Houses.Add(e);
+        await context.SaveChangesAsync();
+
+        return EntityToDto(e);
+    }
+
+    public async Task<HouseDetailDto> Update(HouseDetailDto dto)
+    {
+        // var e = await context.Houses.SingleOrDefaultAsync(h => h.Id == house.Id);
+        var entity = await context.Houses.FindAsync(dto.Id);
+
+        if (entity == null)
+        {
+            throw new ArgumentException($"Error upating house - id {dto.Id} not found");
+        }
+
+        DtoToEntity(dto, entity);
+        context.Entry(entity).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return EntityToDto(entity);
+    }
+
+    public async Task Delete(int id)
+    {
+        var entity = await context.Houses.FindAsync(id);
+
+        if (entity == null)
+        {
+            throw new ArgumentException($"Error deleting house - id {id} not found");
+        }
+
+        context.Houses.Remove(entity);
+        await context.SaveChangesAsync();
     }
 }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -40,5 +42,39 @@ app.MapGet("/house/{houseId:int}", async (IHouseRepository repository, int house
 
     return Results.Ok(house);
 }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+
+app.MapPost("/houses", async (IHouseRepository repository, [FromBody]HouseDetailDto house) => {
+
+    var newHouse = await repository.Add(house);
+    
+    return Results.Created($"/house/{newHouse.Id}", newHouse);
+}).Produces<HouseDto>(StatusCodes.Status201Created);
+
+// app.MapPut("/houses/{houseId:int}", async (IHouseRepository repository, int houseId, [FromBody]HouseDetailDto house) => {
+app.MapPut("/houses", async (IHouseRepository repository, [FromBody]HouseDetailDto house) => {
+    // var e = await repository.Get(house.Id);
+
+    if(await repository.Get(house.Id) == null)
+    {
+        return Results.Problem($"House with ID {house.Id} not found", statusCode: 404);
+    }
+
+    var updatedHouse = await repository.Update(house);
+
+    return Results.Ok(updatedHouse);
+}).Produces<HouseDto>(StatusCodes.Status200OK).ProducesProblem(404);
+
+app.MapDelete("houses/{houseId:int}", async (IHouseRepository repository, int houseId) => {
+    var e = await repository.Get(houseId);
+
+    if(e == null)
+    {
+        return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
+    }
+
+    await repository.Delete(houseId);
+
+    return Results.NoContent();
+}).Produces(StatusCodes.Status204NoContent).ProducesProblem(404);
 
 app.Run();
