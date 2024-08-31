a08917f276148e1bc7112ef48a050fcc051e6d37
Leigh Hunt <leigh@venari.co.nz>
2024-09-01 11:39:43 +1200

    Bid routes, and web extensions classes

--- Api/Program.cs
+++ Api/Program.cs
@@ -27,64 +27,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/houses", (IHouseRepository repository) => repository.GetAll()).Produces<List<HouseDto>>(StatusCodes.Status200OK);
-app.MapGet("/house/{houseId:int}", async (IHouseRepository repository, int houseId) => {
-    var house = await repository.Get(houseId);
-
-    if(house == null)
-    {
-        return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
-    }
-
-    return Results.Ok(house);
-}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
-
-app.MapPost("/houses", async (IHouseRepository repository, [FromBody]HouseDetailDto house) => {
-
-    if(!MiniValidator.TryValidate(house, out var errors))
-    {
-        return Results.ValidationProblem(errors);
-    }
-    var newHouse = await repository.Add(house);
-    
-    return Results.Created($"/house/{newHouse.Id}", newHouse);
-}).Produces<HouseDto>(StatusCodes.Status201Created).ProducesValidationProblem();
-
-// app.MapPut("/houses/{houseId:int}", async (IHouseRepository repository, int houseId, [FromBody]HouseDetailDto house) => {
-app.MapPut("/houses", async (IHouseRepository repository, [FromBody]HouseDetailDto house) => {
-    // var e = await repository.Get(house.Id);
-
-    if(!MiniValidator.TryValidate(house, out var errors))
-    {
-        return Results.ValidationProblem(errors);
-    }
-    if(await repository.Get(house.Id) == null)
-    {
-        return Results.Problem($"House with ID {house.Id} not found", statusCode: 404);
-    }
-
-    var updatedHouse = await repository.Update(house);
-
-    return Results.Ok(updatedHouse);
-}).Produces<HouseDto>(StatusCodes.Status200OK).ProducesProblem(404).ProducesValidationProblem();
-
-app.MapDelete("houses/{houseId:int}", async (IHouseRepository repository, int houseId) => {
-    var e = await repository.Get(houseId);
-
-    if(e == null)
-    {
-        return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
-    }
-
-    await repository.Delete(houseId);
-
-    return Results.NoContent();
-}).Produces(StatusCodes.Status204NoContent).ProducesProblem(404);
+app.MapHouseEndpoints();
+app.MapBidEndpoints();
 
 app.Run();
---
+++ Api/WebApplicationBidExtensions.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using MiniValidation;
+
+public static class WebApplicationBidExtensions
+{
+    public static void MapBidEndpoints(this WebApplication app)
+    {
+        app.MapGet("house/{houseId:int}/bids", async (IBidRepository bidRepository, IHouseRepository houseRepository, int houseId) => {
+
+            if(await houseRepository.Get(houseId) == null)
+            {
+                return Results.Problem($"House with ID {houseId} not found", statusCode: StatusCodes.Status404NotFound);
+            }
+            var bids = await bidRepository.GetBidsForHouse(houseId);
+
+            return Results.Ok(bids);
+        }).Produces<List<BidDto>>(StatusCodes.Status200OK);
+
+        app.MapPost("house/{houseId:int}/bids", async (IBidRepository bidRepository, IHouseRepository houseRepository, int houseId, [FromBody]BidDto bid) => {
+
+            if(bid.HouseId != houseId)
+            {
+                return Results.Problem($"House ID in URL ({houseId}) does not match House ID in body ({bid.HouseId})", statusCode: StatusCodes.Status400BadRequest);
+            }
+
+            if(await houseRepository.Get(houseId) == null)
+            {
+                return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
+            }
+
+            if(!MiniValidator.TryValidate(bid, out var errors))
+            {
+                return Results.ValidationProblem(errors);
+            }
+
+            var newBid = await bidRepository.Add(bid);
+
+            return Results.Created($"/houses/{newBid.HouseId}/bids/{newBid.Id}", newBid);
+        }).Produces<BidDto>(StatusCodes.Status201Created).ProducesProblem(400).ProducesValidationProblem();
+
+    }
+}
